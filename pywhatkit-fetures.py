b7f4468b6e10aaeb75b6613b5e4b6320c8ac2f04
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-26 01:05:00 +0530

    updates

---
+++ pywhatkit-fetures.py
@@ -0,0 +1,12 @@
+import pywhatkit as kit
+
+# kit.search('Apple')
+# kit.info("Ironman",lines=2)
+
+
+#kit.image_to_ascii_art("sage.jpeg", "sage.txt")
+
+
+#kit.text_to_handwriting("Hello everyone, this is my new handwritting", rgb=[0, 0, 0])
+
+# kit.playonyt("Believer")
--- whatsapp-bot.py
+++ whatsapp-bot.py
@@ -1,3 +1,3 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg('+918178973782', 'Lets play valorant', 14, 30)
+pywhatkit.sendwhatmsg('+91xxxxxxxxxx', 'Lets play valorant', 14, 30)
